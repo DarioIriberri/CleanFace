c95e62190b1ed3616fbf22fa0dbaa2485de4b4dc
Dazz <dazzmatazz@gmail.com>
2026-08-15 04:14:52 +0200

    New Order

--- CleanFaceKms/output/copyBin.ps1
+++ CleanFaceKms/output/copyBin.ps1
@@ -1,5 +1,5 @@
 $shell = New-Object -ComObject Shell.Application
-$src = $shell.NameSpace('C:\Programs\EasyFace\CleanFaceKms\output').ParseName('CleanFaceKms.face')
+$src = $shell.NameSpace('C:\Programs\EasyFace\CleanFace\CleanFaceKms\output').ParseName('CleanFaceKms.face')
 $dst = $shell.NameSpace('shell:::{20D04FE0-3AEA-1069-A2D8-08002B30309D}').Items() | Where-Object { $_.Name -like '*Redmi Note 14*' }
 $storage = $dst.GetFolder.Items() | Where-Object { $_.Name -like '*Internal*' -or $_.Name -like '*Almacenamiento*' }
 $docs = $storage.GetFolder.Items() | Where-Object { $_.Name -eq 'Documents' }
